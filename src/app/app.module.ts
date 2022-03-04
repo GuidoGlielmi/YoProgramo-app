c792ef4b1d4eb95a8cd1ce0c2ee140823cbfbf48
guidoglielmi <guidoglielmi@gmail.com>
2022-03-04 18:46:03 -0300

    Created section wrapper and dropDown component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { PersonalInfoComponent } from './components/personal-info/personal-info.component';
 import { ExperienceComponent } from './components/experience/experience.component';
+import { DropDownComponent } from './components/drop-down/drop-down.component';
+import { SectionComponent } from './components/section/section.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, PersonalInfoComponent, ExperienceComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    PersonalInfoComponent,
+    ExperienceComponent,
+    DropDownComponent,
+    SectionComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
