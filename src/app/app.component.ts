3f5625b9dc0428529dd0f231d32b6e1350f37e30
guidoglielmi <guidoglielmi@gmail.com>
2022-03-10 03:53:07 -0300

    Added nav bar drop down functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,6 +7,15 @@ import { Component, HostListener, ViewChild } from '@angular/core';
 })
 export class AppComponent {
   title = 'YoProgramo-app';
+  @ViewChild('socialOnCell') socialOnCell: any;
+
+  showSocialOnCell = false;
+  toggleSocialOnCell() {
+    if (this.showSocialOnCell) {
+      this.socialOnCell.nativeElement.style.height = '5vh';
+    } else this.socialOnCell.nativeElement.style.height = 0;
+    this.showSocialOnCell = !this.showSocialOnCell;
+  }
 }
 /*
 Los templates de Angular son fragmentos de HTML dinámicos, y cuando Angular los renderiza, transforma el DOM de acuerdo con las instrucciones dadas por las directivas. Una directiva es una clase con un decorador @Directive() (del cual extiende el decorador @Component). Además de los componentes, existen otros dos tipos de directivas: estructural y atributo.
