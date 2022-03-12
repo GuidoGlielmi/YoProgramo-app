ef329dbebce7a322340f2b636598ab95f6a94b0f
Guido Glielmi <guidoglielmi@gmail.com>
2022-03-12 14:12:28 -0300

    Hardcoded info modified, css details fixed

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -8,16 +8,16 @@ import { Component, Input, OnInit } from '@angular/core';
 export class SkillsComponent implements OnInit {
   @Input() size: number = 0;
   skills = [
-    { title: 'tuvieja', ability: 80 },
-    { title: 'tuvieja', ability: 60 },
-    { title: 'tuvieja', ability: 75 },
-    { title: 'tuvieja', ability: 25 },
-    { title: 'tuvieja', ability: 10 },
+    { title: 'Resilience', ability: 60 },
+    { title: 'Empathy', ability: 60 },
+    { title: 'Active listening', ability: 75 },
+    { title: 'Proactive', ability: 60 },
+    { title: 'Creativity', ability: 80 },
   ];
   languages = [
     { title: 'Español', ability: 100 },
     { title: 'English', ability: 80 },
-    { title: '日本語 / Japanese', ability: 25 },
+    { title: '日本語 / Japanese', ability: 20 },
   ];
   ngOnInit(): void {}
 }
